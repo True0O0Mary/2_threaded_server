e095efd56d2ffd3eb0b23081df2b8a7bbfc76c4e
True0O0Mary <truefalsemary@gmail.com>
2022-10-06 15:40:07 +0300

    Done 2 task. Client in new thread. - added threading to server.py

--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 import socket
+import threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+def main():
+    sock = socket.socket()
+    sock.bind(('', 9090))  
+    sock.listen(0)  
+    conn, addr = sock.accept()
+    print(addr)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    msg = ''
 
-print(msg)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        msg += data.decode()
+        conn.send(data)
 
-conn.close()
+    print(msg)
+    conn.close()
+
+
+t = threading.Thread(target=main)
+t_lock = threading.Lock()
+with t_lock:
+    t.start()
