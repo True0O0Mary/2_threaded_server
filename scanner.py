d55d5dc17ab9e8886f2ef9b5c3dac920cf724117
True0O0Mary <truefalsemary@gmail.com>
2022-10-06 15:03:21 +0300

    Done 1.3. Progress bar. - added tqdm to scanner.py

--- scanner.py
+++ scanner.py
@@ -1,6 +1,8 @@
 import socket
 from threading import Thread, Lock
 from time import sleep
+from progress.bar import IncrementalBar
+from tqdm import tqdm
 
 N = 2**16 
 
@@ -10,8 +12,8 @@ port_lock = Lock()
 def main(n):   
     for port in range(256):
         sock = socket.socket()
+        progress_bar.update()
         try:
-            # print(port+n)
             with port_lock:
                 sock.connect(('127.0.0.1', port+n))
                 print("Порт", port+n, "открыт")
@@ -20,5 +22,7 @@ def main(n):
         
 t = [Thread(target=main, args=[i])
     for i in range(0, N, N//256)]
+
+progress_bar = tqdm(N, desc="Progress bar")
 
 [t1.start() for t1 in t]
