0ead3ac1e0123115d0490a1424f181bf8dc81205
True0O0Mary <truefalsemary@gmail.com>
2022-10-07 14:29:09 +0300

    Done 3 task. Multi-user ChatRoom. - added thread to every new user - added authorization.json with passwords - added history.csv (contain message history)

--- client.py
+++ client.py
@@ -1,16 +1,50 @@
 import socket
+from threading import Thread
+from datetime import datetime
 from time import sleep
+from history import *
+from authorization import *
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('127.0.0.1', 9090))
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 9090
+sep = "<SEP>"
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+def listen():
+    while True:
+        message = sock.recv(1024).decode()
+        print(message)
 
-data = sock.recv(1024)
 
-sock.close()
+if __name__ == "__main__":
+    # Connection to server
+    sock = socket.socket()
+    sock.connect((SERVER_HOST, SERVER_PORT))
+    print(f"Connected to {SERVER_HOST}:{SERVER_PORT}")
+    # name = input("Input your name: ")
+    while True:
+        name = auto()
+        if name:
+            print("Success!!!")
+            break
+        print("Incorrect name or password")
 
-print(data.decode())
+    # Making a thread for listening to messages
+    t = Thread(target=listen)
+    t.daemon = True
+    t.start()
+
+    print("Greet everyone in the chat room")
+    while True:
+        to_send = input()
+        if to_send == "q":
+            break
+        date_now = datetime.now().strftime("%d.%m.%y %H:%M:%S")
+
+        write_message(date_now, name, to_send)
+
+        to_send = f"[{date_now}] {name}{sep}{to_send}"
+        sock.send(to_send.encode())
+
+    sock.close()
+
+
--- server.py
+++ server.py
@@ -1,28 +1,48 @@
 import socket
-import threading
+from threading import Thread
+from authorization import *
+from history import *
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 9090
+
+client_sockets = set()
+
+sep = "<SEP>"
 
 
-def main():
-    sock = socket.socket()
-    sock.bind(('', 9090))  
-    sock.listen(0)  
-    conn, addr = sock.accept()
-    print(addr)
-
-    msg = ''
-
+def listen(cs):
     while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        msg += data.decode()
-        conn.send(data)
+        try:
+            msg = cs.recv(1024).decode()
+        except Exception as e:
+            print(f"!*! Client {cs} no longer connected")
+            client_sockets.remove(cs)
+        else:
+            msg = msg.replace(sep, ": ")
 
-    print(msg)
-    conn.close()
+        for client_socket in client_sockets:
+            client_socket.send(msg.encode())
 
 
-t = threading.Thread(target=main)
-t_lock = threading.Lock()
-with t_lock:
-    t.start()
+if __name__ == "__main__":
+
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((SERVER_HOST, SERVER_PORT))
+    sock.listen(10)
+    print(f"!*! Start listening as {SERVER_HOST}: {SERVER_PORT}")
+    write_empty_row()
+    while True:
+        conn, addr = sock.accept()
+
+
+        print(f"!*! {addr} connected!")
+        client_sockets.add(conn)
+        t = Thread(target=listen, args=(conn,))
+        t.daemon = True
+        t.start()
+
+    for cs in client_sockets:
+        cs.close()
+    sock.close()
